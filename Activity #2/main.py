c6c85580374de274667529c9ae92cbfb6c8129c4
Vonn Harold Mamaril <118141177+theunknownsky@users.noreply.github.com>
2024-06-10 21:26:07 +0800

    Delete Activity #2 directory

--- Activity #2/main.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-from registration import Registration
-
-if __name__ == "__main__":
-    mamaril_app = QApplication([])
-    mamaril_window = Registration()
-    mamaril_window.show()
-    mamaril_app.exec()
